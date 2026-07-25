b90e6fa51ac94b3a11d9de12d1c760f5cbdd7855
German <gmbigrevich@gmail.com>
2026-07-25 16:57:27 -0300

    Update email obfuscation and user details

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ document.addEventListener('DOMContentLoaded', () => {
             e.preventDefault();
             
             // Reemplaza esto con tus datos reales. Al estar separados, los bots simples no los unen.
-            const user = 'contacto';
-            const domain = 'mateosecurity.com';
+            const user = 'parz1val93';
+            const domain = 'proton.me';
             
             // Construcción y ejecución segura
             window.location.href = `mailto:${user}@${domain}`;
